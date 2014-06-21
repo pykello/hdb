5ec34768eef2e17218fe941aa00a821efde58ce0
Hadi Moshayedi <hadi@moshayedi.net>
2014-06-22 01:27:36 +0300

    launch-hdb now sends bucket_map to servers.

--- launch-hdb.py
+++ launch-hdb.py
@@ -3,8 +3,10 @@
 import os
 import socket
 import sys
+import time
 
 import main
+import json
 from daemon import Daemon
 
 class HDBDaemon(Daemon):
@@ -33,12 +35,31 @@ def find_open_port(start_from):
     return port
 
 
+def get_bucket_map(ports):
+    bucket_map = [['localhost', ports[i % len(ports)]] for i in range(4096)]
+    return bucket_map
+
+
+def send_bucket_map(hostname, port, bucket_map):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((hostname, port))
+    msg = {"code": "BucketMapUpdate", "bucket_map": bucket_map}
+    s.send(json.dumps(msg) + "$$$")
+
+    response = json.loads(s.recv(1024))
+    if response["code"] != "OK":
+        print "Error in sending bucket map to (localhost, %d)" % (port, )
+
+    s.close()
+
+
 if __name__ == "__main__":
     instance_count = int(sys.argv[1])
     node_max = sys.argv[2]
     rel_max = sys.argv[3]
 
     port = 5430
+    ports = []
     for i in range(instance_count):
         port = find_open_port(port)
         pid = os.fork()
@@ -48,6 +69,13 @@ if __name__ == "__main__":
             daemon.start()
         else:
             print port
+            ports.append(port)
             port += 1
+
+    time.sleep(2)
+
+    bucket_map = get_bucket_map(ports)
+    for port in ports:
+        send_bucket_map('localhost', port, bucket_map)
 
     sys.exit(0)
